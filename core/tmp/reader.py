9bdb9018726dca9e2bd8707e2397945df81b0000
yuguno <31914737+yuguno@users.noreply.github.com>
2017-09-14 17:20:07 +0900

    9/14
    完成

--- core/tmp/reader.py
+++ core/tmp/reader.py
@@ -7,8 +7,11 @@ def none_detector(obj):
         return obj
 
 f = open('./2016_6scraping.csv', 'r')
-cnt = 0
-pattern = []
+cnt = 1
+pattern = {}
+popular = {}
+
+
 for line in f:
     line = line.rstrip()
     l = line.split(',')
@@ -17,5 +20,20 @@ for line in f:
     l6 = none_detector(l[6])
     l7 = none_detector(l[7])
     l8 = none_detector(l[8])
-    patt = 'top1: '+l5+'\ttop2: '+l6+'\ttop3: '+l7+'\tbottom: '+l8
-    cnt = cnt + 1
+    patt = 'top1: '+l5+' top2: '+l6+' top3: '+l7+' bottom: '+l8
+    print patt
+
+    for i in range(cnt):
+        try:
+            if pattern[cnt] == patt:
+                try:
+                    popular[cnt] += 1
+                except KeyError:
+                    popular[cnt] = 1
+        except KeyError:
+            pattern[cnt] = patt
+            popular[cnt] = 1
+            cnt += 1
+
+print pattern
+print popular
