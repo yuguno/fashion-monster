71877b039878479ddd29c62210465eb2da41fbf9
yuguno <31914737+yuguno@users.noreply.github.com>
2017-09-14 16:22:47 +0900

    Confirm
    Confirmテスト

--- hello.py
+++ hello.py
@@ -54,11 +54,11 @@ def callback():
     return 'OK'
 
 
-@handler.add(MessageEvent, message=TextMessage)
-def handle_message(event):
-    line_bot_api.reply_message(
-        event.reply_token,
-        TextSendMessage(text=event.message.text))
+#@handler.add(MessageEvent, message=TextMessage)
+#def handle_message(event):
+#    line_bot_api.reply_message(
+#        event.reply_token,
+#        TextSendMessage(text=event.message.text))
 
 
 @handler.add(MessageEvent, message=ImageMessage)
@@ -96,7 +96,7 @@ def handle_location(event):
 #                TextSendMessage(text="保存")
 #            )
 
-#@handler.add(MessageEvent, message=TextMessage)
+@handler.add(MessageEvent, message=TextMessage)
 #def handle_message(event):
 #    line_bot_api.reply_message(
 #        event.reply_token,
